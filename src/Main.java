9a74627ff027602b6535ae09deb0bae7487975b2
root <nima10khodaveisi@gmail.com>
2020-03-10 19:52:05 +0330

    first commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ import java.util.Scanner ;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in) ;
+        String str = sc.nextLine() ;
+        System.out.println(str);
+        Scanner sc2 = new Scanner(System.in) ;
+        str = sc.nextLine() ;
+        System.out.println(str);
     }
 }
